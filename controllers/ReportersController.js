ef6f6762c2b53660f9801c69475cda9b3807bc7e
Debbie <cinemaestro@yahoo.com>
2015-02-27 13:58:59 -0800

    add ability to search and edit beats

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -6,9 +6,9 @@ newspaperTracker.controller('ReportersCtrl', function ReportersCtrl(
     BeatsFactory.beats, $stateParams.beatId);
     $scope.BeatsFactory = BeatsFactory;
 
-  $scope.reporter = UtilitiesFactory.findById(
-    BeatsFactory.beats, $stateParams.reporterId);
-    $scope.BeatsFactory = BeatsFactory;
+  // $scope.reporter = UtilitiesFactory.findById(
+  //   BeatsFactory.beats, $stateParams.reporterId);
+  //   $scope.BeatsFactory = BeatsFactory;
 
   $scope.addReporter = function() {
     $scope.beat.reporters.push({ name: $scope.reporterName, yearsEmployed: $scope.reporterYearsEmployed, birthday: $scope. reporterBirthday  });
